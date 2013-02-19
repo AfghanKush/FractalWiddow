8d7a8308ffc3ffcd9baab6ddc4a139c157a8f21f
Louhichi Younes El Mehdi <yshino22@gmail.com>
2013-02-19 12:39:05 +0100

    methode point

--- src/ch/epfl/flamemaker/geometry2d/Point.java
+++ src/ch/epfl/flamemaker/geometry2d/Point.java
@@ -1,5 +1,37 @@
 package ch.epfl.flamemaker.geometry2d;
+import   java.lang.Math.*;
+
+
 
 public class Point {
+private double x;
+private double y;
+Point(double x, double y){
+	this.x=x;
+	this.y=y;
+}
+
+public double x(){
+	return x;
+}
+
+public double y(){
+	return y;
+}
+
+public double r(){
+	return Math.sqrt(x*x+y*y);
+}
+
+public double theta(){
+	return Math.atan2(x, y);
+}
+
+public String toString(){
+	return "("+x+", "+y+")";
+}
+
+public static Point ORIGIN= new Point(0,0);
 
 }
+
