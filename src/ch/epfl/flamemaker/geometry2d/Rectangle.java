2e7c9908172d4137693da1a50e99bf2f7eb33712
Louhichi Younes El Mehdi <yshino22@gmail.com>
2013-02-19 14:26:38 +0100

    encore une méthode a finir sur rectangle
    extendaspectratio! cherche l'amnésia mon frère

--- src/ch/epfl/flamemaker/geometry2d/Rectangle.java
+++ src/ch/epfl/flamemaker/geometry2d/Rectangle.java
@@ -1,52 +1,58 @@
 package ch.epfl.flamemaker.geometry2d;
 
-public class Rectangle {
-	private Point center;
-	private double height;
-	private double width;
+public class Rectangle { //classe du rect
+	private Point center; //centre du rectangle 
+	private double height; //hateur
+	private double width; // largeur
 
-	Rectangle(Point center, double height, double width) {
+	Rectangle(Point center, double height, double width) { //constructeur du rectangle
 		this.center = center;
 		this.height = height;
 		this.width = width;
 		// *amnesia* lever l'exception
 	}
 	
-	public double left() {
+	public double left() { //renvoie le point du rectangle le plus a gauche
 		return center.x() - (width/2);
 	}
 	
-	public double right() {
+	public double right() { //renvoie le point du rectangle le plus a droite
 		return center.x() + (width/2);
 	}
 	
-	public double bottom() {
-		return center.y() - (width/2);
+	public double bottom() { //renvoie le point du rectangle le plus bas
+		return center.y() - (height/2);
 	}
 	
-	public double top() {
-		return center.y() - (width/2);
+	public double top() { //renvoie le point du rectangle le plus haut
+		return center.y() + (height/2);
 	}
 	
-	public double width() {
+	public double width() { //renvoie la largeur du rectangle
 		return width;
 	}
 
-	public double height() {
+	public double height() { //renvoie la hateur du rectangle
 		return height;
 	}
 	
-	public Point center() {
+	public Point center() { //renvoie le point du centre du rectangle
 		return center;
 	}
 	
-	public boolean contains(Point p) {
-		
+	public boolean contains(Point p) { //vérifie si le point est a l intérieur du rectangle
+		if( p.x()>=left() && p.x()<right() && p.y()<=top() && p.y()>bottom())
+		{return true;}
+		else return false;  
 	}
 	
-	public double double aspectRatio() {
+	public double aspectRatio() { //ratio entre largeur est hauteur, pour comparer la taille des rectangle
 		 return width/height;
 	 }
+	
+	Rectangle expandToAspectRatio(double aspectRatio){ //compare les rectangle qui on le meme centre *amnesia* 
+		//à finir!!
+	}
 	
 	public String toString() {
 		return "(" + center.toString() + "," + width + "," + height + ")";
