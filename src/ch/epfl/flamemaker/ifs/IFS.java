b1f1d4911cc0ef07060663a8d51a55bd7602dd79
Louhichi Younes El Mehdi <yshino22@gmail.com>
2013-03-09 18:44:06 +0100

    j ai encore mis quelques notes, essaye de finir
    l'etape 2

--- src/ch/epfl/flamemaker/ifs/IFS.java
+++ src/ch/epfl/flamemaker/ifs/IFS.java
@@ -22,13 +22,19 @@ public class IFS{ //implements List<AffineTransformation> {
 	public IFSAccumulator compute(Rectangle frame, int width, int height, int density) {
 		Point p=Point.ORIGIN;
 		for(int j=0; j<20; j++){
-			int i=Randomint.randomno.nextInt(n-1); //entier aléatoire en 0 et n-1 // dernière valeure non-incluse
+			int i=Randomint.randomno.nextInt(ifsList.size()); //entier aléatoire en 0 et n-1 // dernière valeure non-incluse // le bon n??
 			//p = F[i](p)  ----> traduire en java
 		}
 		
+		//nombre d intération= H(en nombre de case) x W (en nombre de case) x Density
+		int m= (int)height*(int)width*(int)density;
+		
+		
 		IFSAccumulator S;
+		
+		
 		for(int j=0; j<m; j++){
-			int i=0; //entier aléatoire en 0 et n-1
+			int i=Randomint.randomno.nextInt(ifsList.size()); //entier aléatoire en 0 et n-1 // dernière valeure non-incluse // le bon n??
 			//p = F[i](p)  ----> traduire en java
 			//S=S+p; ---> traduire en java
 		}
