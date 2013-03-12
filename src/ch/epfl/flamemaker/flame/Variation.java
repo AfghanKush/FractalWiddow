d8a1ec8a8d997607a0646655546e2d58c262d6c2
Louhichi Younes <yshino22@gmail.com>
2013-03-12 21:33:17 +0100

    plus qu'une méthode et la classe de test
    bientot finis :D

--- src/ch/epfl/flamemaker/flame/Variation.java
+++ src/ch/epfl/flamemaker/flame/Variation.java
@@ -26,47 +26,53 @@ public abstract class Variation implements Transformation
     	return this.index; 
     }
     
-    
-    abstract public Point transformPoint(Point p);
-
     public final static List<Variation> ALL_VARIATIONS =
         Arrays.asList(new Variation(0, "Linear") 
         {
             public Point transformPoint(Point p) 
             {
-                // …
+        		return p;
             }
         },new Variation(1,"Sinusoidal")
         {
         	public Point transformPoint(Point p)
         	{
-        		// ...
+        		double x=Math.sin(p.x());
+        		double y=Math.sin(p.y());
+        		return new Point(x,y);
         	}
         },new Variation(2,"Spherical")
         {
         	public Point transformPoint(Point p)
         	{
-        		// ...
+        		double x=p.x()/p.r()*p.r();
+        		double y=p.y()/p.r()*p.r();
+        		return new Point(x,y);
         	}
         },new Variation(3,"Swirl")
         {
         	public Point transformPoint(Point p)
         	{
-        		
+        		double x=p.x()*Math.sin(p.r()*p.r())-p.y()*Math.cos(p.r()*p.r());
+        		double y=p.x()*Math.cos(p.r()*p.r())+p.x()*Math.cos(p.r()*p.r());
+        		return new Point(x,y);
         	}
         },new Variation(4,"Horseshoe")
         {
         	public Point transformPoint(Point p)
         	{
-        		
+        		double x=(p.x()-p.y())*(p.x()+p.y())/p.r();
+        		double y=p.x()*2*p.y()/p.r();
+        		return new Point(x,y);
         	}
         },new Variation(5,"Bubble")
         {
         	public Point transformPoint(Point p)
         	{
-        		
+        		double x=4*p.x()/(p.r()*p.r()+4);
+        		double y=4*p.y()/(p.r()*p.r()+4);
+        		return new Point(x,y);
         	}
         }
-            // …
-            );
+            ); //ferme la liste des variations.
 }
